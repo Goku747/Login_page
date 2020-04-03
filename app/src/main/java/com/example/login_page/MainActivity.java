7bdae659102ccdba44980682c4be9efcba06ba79
Gokul <47682121+Goku747@users.noreply.github.com>
2020-04-03 18:33:02 +0530

    Logic added for UserLogin

--- app/src/main/java/com/example/login_page/MainActivity.java
+++ app/src/main/java/com/example/login_page/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.login_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText Username;
+    private EditText Password;
+    private CardView LoginCardView;
+    private TextView Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void authenticateLogin(View view) {
+        if (Username.getText().toString().equals("admin") &&
+                Password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Hello admin!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Seems like you 're not admin!",
+                    Toast.LENGTH_SHORT).show();
+            }
+        }
 }
